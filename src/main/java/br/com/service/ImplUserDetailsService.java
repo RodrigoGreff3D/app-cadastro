ec91e754b6c8290e811a3fd70af9df50f50164c5
Rodrigo Dias Ramos <greff3d@hotmail.com>
2021-08-24 16:45:07 -0300

    Create ImplUserDetailsService.java

---
+++ src/main/java/br/com/service/ImplUserDetailsService.java
@@ -0,0 +1,22 @@
+package br.com.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.repository.UsuarioRepository;
+
+@Service
+public class ImplUserDetailsService implements UserDetailsService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return null;
+	}
+
+}
